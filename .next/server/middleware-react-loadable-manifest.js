7cd4eec71ad01d940762839d7c7d53d06fc6a12b
Agda Lopes <domingoslopesagda@gmail.com>
2026-05-06 16:00:47 -0100

    Remove arquivos ignorados (.next cache) do repositório

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"components/hero/AvatarCanvas.tsx -> @/components/avatar/AvatarScene\":{\"id\":\"components/hero/AvatarCanvas.tsx -> @/components/avatar/AvatarScene\",\"files\":[\"static/chunks/_app-pages-browser_src_components_avatar_AvatarScene_tsx.js\"]}}"
